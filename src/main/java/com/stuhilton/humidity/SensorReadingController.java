96e77d5f0bfb3843f7739617552bd8ca32f3a8f4
nullMethod <null.method@gmail.com>
2018-03-19 22:05:25 +0000

    Convert endpoint to post

--- src/main/java/com/stuhilton/humidity/SensorReadingController.java
+++ src/main/java/com/stuhilton/humidity/SensorReadingController.java
@@ -1,18 +1,17 @@
 package com.stuhilton.humidity;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
 public class SensorReadingController {
 
-    @RequestMapping(value = "/sensorReading", method = GET)
-    public SensorReading addSensorReading(
-            @RequestParam(value="reading") double reading,
-            @RequestParam(value="type") String type) {
-        return new SensorReading(reading, type);
+    @RequestMapping(value = "/sensorReading", method = POST)
+    public SensorReading addSensorReading(@RequestBody SensorReading reading) {
+        return new SensorReading(0.00, null);
     }
 }
